9d6933d1fe297df7dfebd00d0bf24f993bfba912
Arseniy <krutoi.arseniy@gmail.com>
2025-01-18 17:34:50 +0300

    feat: finish 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
 const path = require('node:path');
-
 const pathFolder = path.dirname('04-copy-directory/files/test-js.js');
-// console.log(pathFolder);
-// console.log(pathFileCopy);
-copyDir = () => {
-  fs.readdir(pathFolder, (err, files) => {
+const copyDir = () => {
+  fs.readdir(pathFolder, (err, files1) => {
     if (err) {
       console.error('error');
     } else {
-      files.forEach((file) => {
+      files1.forEach((file) => {
         const pathFile = path.join('04-copy-directory', 'files', file);
         const pathFileCopy = path.join('04-copy-directory', 'files-copy', file);
         fs.copyFile(pathFile, pathFileCopy, (err) => {
@@ -18,15 +15,29 @@ copyDir = () => {
           }
         });
       });
+      fs.readdir('04-copy-directory/files-copy/', (err, files2) => {
+        if (files2.length !== files1.length) {
+          files2.forEach((file2) => {
+            if (!files1.includes(file2)) {
+              fs.rm(`04-copy-directory/files-copy/${file2}`, (err) => {
+                console.log(err);
+              });
+            }
+          });
+        }
+      });
     }
   });
 };
 copyDir();
 
-fs.mkdir(
-  path.join('04-copy-directory', 'files-copy'),
-  { recursive: true },
-  (err) => {
-    console.log(err);
-  },
-);
+const createDelFolder = () => {
+  fs.mkdir(
+    path.join('04-copy-directory', 'files-copy'),
+    { recursive: true },
+    (err) => {
+      err;
+    },
+  );
+};
+createDelFolder();
